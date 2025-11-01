6c4562cf3494ed4bb503a47355d5a9d682d2944a
Lanktor <64542490+Lanktor@users.noreply.github.com>
2025-11-01 16:59:11 -0400

    Update 1

--- Prototype.h
+++ Prototype.h
@@ -3,3 +3,6 @@
 INT InitializeApplication(PGAME_INFO GIptr);
 
 
+PSDL_Texture TEXTURE_Load(PGAME_INFO GIptr, PCCHAR FName);
+
+INT TEXT_WriteText(PGAME_INFO GIptr, SDL_Color Color, PSDL_FRect Dest, const char *Text, INT Scale);
